fa635fc31159dfe8315063176978dd5d0ad268e0
toniliu <toniliu672@gmail.com>
2024-09-20 17:57:52 +0800

    add share news

--- news_detail.php
+++ news_detail.php
@@ -35,6 +35,84 @@ try {
 }
 ?>
 
+<!-- Share Social Media -->
+<?php
+function generateShareLinks($url, $title, $content, $imageUrl = null) {
+    $encodedUrl = urlencode($url);
+    $encodedTitle = urlencode($title);
+    
+    // Create a compelling description
+    $description = createCompellingDescription($content);
+    $encodedDescription = urlencode($description);
+    
+    // Create an engaging WhatsApp message
+    $whatsappMessage = createWhatsAppMessage($title, $description, $url);
+    $encodedWhatsappMessage = urlencode($whatsappMessage);
+    
+    $facebookUrl = "https://www.facebook.com/sharer/sharer.php?u={$encodedUrl}&quote={$encodedTitle}";
+    $instagramUrl = "https://www.instagram.com/";  // Instagram doesn't support direct sharing
+    $whatsappUrl = "https://api.whatsapp.com/send?text={$encodedWhatsappMessage}";
+    
+    return [
+        'facebook' => $facebookUrl,
+        'instagram' => $instagramUrl,
+        'whatsapp' => $whatsappUrl
+    ];
+}
+
+function createCompellingDescription($content) {
+    $cleanContent = strip_tags($content);
+    $words = str_word_count($cleanContent, 1);
+    $shortDescription = implode(' ', array_slice($words, 0, 20));
+    return $shortDescription . "...";
+}
+
+function createWhatsAppMessage($title, $description, $url) {
+    $emoji = getRelevantEmoji($title);
+    $hashtag = createHashtag($title);
+    
+    return "🔥 Berita Terbaru SMKN 1 Bolaang! 🔥\n\n" .
+           "{$emoji} *{$title}*\n\n" .
+           "{$description}\n\n" .
+           "🔗 Baca selengkapnya: {$url}\n\n" .
+           "Jangan lupa share ke teman-temanmu ya! 👍\n" .
+           "{$hashtag}";
+}
+
+function getRelevantEmoji($title) {
+    $keywords = [
+        'prestasi' => '🏆',
+        'lomba' => '🥇',
+        'teknologi' => '💻',
+        'olahraga' => '⚽',
+        'seni' => '🎨',
+        'wisuda' => '🎓'
+    ];
+    
+    foreach ($keywords as $keyword => $emoji) {
+        if (stripos($title, $keyword) !== false) {
+            return $emoji;
+        }
+    }
+    
+    return '📢';  // Default emoji if no keyword matches
+}
+
+function createHashtag($title) {
+    $words = explode(' ', strtolower($title));
+    $hashtag = '#SMKN1Bolaang';
+    if (count($words) > 0) {
+        $hashtag .= ucfirst($words[0]);
+    }
+    return $hashtag;
+}
+
+// Usage:
+$currentUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$shareLinks = generateShareLinks($currentUrl, $news['title'], $news['content']);
+?>
+
+
 <!DOCTYPE html>
 <html lang="id">
 
@@ -47,6 +125,7 @@ try {
 
     <!--=============== REMIXICONS ===============-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.4.0/remixicon.css" crossorigin="">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -217,6 +296,7 @@ try {
                             <span>Like</span>
                             <span id="likeCount" class="ml-2">(<?php echo $like_count; ?>)</span>
                         </button>
+
                     </div>
                     <p class="text-sm mb-4">Dibuat tanggal <?php echo date('d F Y', strtotime($news['created_at'])); ?></p>
                     <?php if ($news['image']): ?>
@@ -226,6 +306,26 @@ try {
                         <?php echo nl2br(htmlspecialchars($news['content'])); ?>
                     </div>
 
+                    <div class="mt-6 mb-6 p-4 flex flex-wrap items-center justify-end gap-4">
+                        <h3 class="text-lg font-bold">Bagikan Artikel :</h3>
+                        <div class="flex flex-wrap gap-4">
+                            <a href="<?php echo $shareLinks['facebook']; ?>" target="_blank" class="hover:opacity-80 transition duration-300" title="Bagikan ke Facebook">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                    <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3 8h-1.35c-.538 0-.65.221-.65.778v1.222h2l-.209 2h-1.791v7h-3v-7h-2v-2h2v-2.308c0-1.769.931-2.692 3.029-2.692h1.971v3z" fill="#1877F2" />
+                                </svg>
+                            </a>
+                            <a href="<?php echo $shareLinks['instagram']; ?>" target="_blank" class="hover:opacity-80 transition duration-300" title="Bagikan ke Instagram">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                    <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" fill="#E4405F" />
+                                </svg>
+                            </a>
+                            <a href="<?php echo $shareLinks['whatsapp']; ?>" target="_blank" class="hover:opacity-80 transition duration-300" title="Bagikan ke WhatsApp">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                    <path d="M12.031 6.172c-3.181 0-5.767 2.586-5.768 5.766-.001 1.298.38 2.27 1.019 3.287l-.582 2.128 2.182-.573c.978.58 1.911.928 3.145.929 3.178 0 5.767-2.587 5.768-5.766.001-3.187-2.575-5.77-5.764-5.771zm3.392 8.244c-.144.405-.837.774-1.17.824-.299.045-.677.063-1.092-.069-.252-.08-.575-.187-.988-.365-1.739-.751-2.874-2.502-2.961-2.617-.087-.116-.708-.94-.708-1.793s.448-1.273.607-1.446c.159-.173.346-.217.462-.217l.332.006c.106.005.249-.04.39.298.144.347.491 1.2.534 1.287.043.087.072.188.014.304-.058.116-.087.188-.173.289l-.26.304c-.087.086-.177.18-.076.354.101.174.449.741.964 1.201.662.591 1.221.774 1.394.86s.274.072.376-.043c.101-.116.433-.506.549-.68.116-.173.231-.145.39-.087s1.011.477 1.184.564.289.13.332.202c.045.072.045.419-.1.824zm-3.423-14.416c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm.029 18.88c-1.161 0-2.305-.292-3.318-.844l-3.677.964.984-3.595c-.607-1.052-.927-2.246-.926-3.468.001-3.825 3.113-6.937 6.937-6.937 1.856.001 3.598.723 4.907 2.034 1.31 1.311 2.031 3.054 2.03 4.908-.001 3.825-3.113 6.938-6.937 6.938z" fill="#25D366" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
 
 
                     <!-- Comment form -->
